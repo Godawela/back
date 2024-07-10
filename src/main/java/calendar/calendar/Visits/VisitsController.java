a4354c3b5ef4d1b855f4c2d3201c07acca228b8f
Godawela <godawelaus.21@uom.lk>
2024-07-10 07:55:51 +0530

    final

--- src/main/java/calendar/calendar/Visits/VisitsController.java
+++ src/main/java/calendar/calendar/Visits/VisitsController.java
@@ -1,6 +1,7 @@
 package calendar.calendar.Visits;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 // import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin(origins="*",maxAge=3600)
 @RestController
 @RequestMapping("/visits")
 public class VisitsController {
